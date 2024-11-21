a4892fde465a20059ccbf485af2c4f7738313f85
Marcy Thany <marcythany@gmail.com>
2024-11-20 21:06:54 -0300

    feat: calculadora pronta.

--- calculadora/main.js
+++ calculadora/main.js
@@ -1,7 +1,26 @@
-import './style.css';
+function outcome() {
+	let numOne = Number(document.getElementById('num-one').value);
+	let numTwo = Number(document.getElementById('num-two').value);
+	let total = 0;
 
-document.querySelector('#app').innerHTML = `
-  <div>
+	if (document.getElementById('box1').checked) {
+		total = numOne + numTwo;
+	} else if (document.getElementById('box2').checked) {
+		total = numOne - numTwo;
+	} else if (document.getElementById('box3').checked) {
+		total = numOne * numTwo;
+	} else if (document.getElementById('box4').checked) {
+		total = numOne / numTwo;
+	}
+	document.getElementById('resultArea').innerHTML = 'Result: ' + String(total);
+}
 
-  </div>
-`;
+function reset() {
+	document.getElementById('num-one').value = '';
+	document.getElementById('num-two').value = '';
+	document.getElementById('resultArea').innerHTML = '';
+	document.getElementById('box1').checked = false;
+	document.getElementById('box2').checked = false;
+	document.getElementById('box3').checked = false;
+	document.getElementById('box4').checked = false;
+}
